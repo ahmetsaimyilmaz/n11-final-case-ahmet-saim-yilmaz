5b418f9b7d34db56e14c0a8f7d81fcfa3f9f9dc9
Ahmet Saim Yılmaz <ahmetiesto@gmail.com>
2024-03-09 14:58:59 +0300

    Inital commit for the project

---
+++ src/test/java/n11/n11finalcaseahmetsaimyilmaz/N11FinalCaseAhmetSaimYilmazApplicationTests.java
@@ -0,0 +1,13 @@
+package n11.n11finalcaseahmetsaimyilmaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N11FinalCaseAhmetSaimYilmazApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

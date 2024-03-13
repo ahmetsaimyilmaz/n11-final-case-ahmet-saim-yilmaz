7684fe022bc01a8d0e090c3e1bec9938b4cfb176
Ahmet Saim Yılmaz <ahmetiesto@gmail.com>
2024-03-14 00:22:12 +0300

    Code Revision

--- src/main/java/n11/n11finalcaseahmetsaimyilmaz/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package n11.n11finalcaseahmetsaimyilmaz.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
--- src/main/java/n11/n11finalcaseahmetsaimyilmaz/userReview/UserReviewRepository.java
+++
@@ -1,6 +0,0 @@
-package n11.n11finalcaseahmetsaimyilmaz.userReview;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
-}
